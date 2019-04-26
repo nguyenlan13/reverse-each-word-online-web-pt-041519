8a04c78a2b89c0ea9e30730070e59481b89f619f
Lan Nguyen <nguyenlan13@gmail.com>
2019-04-26 04:58:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(array)
   array_new = array.split(" ")
   array_revers = []
   array_reverse = array_new.each do |array|
-    array_reverse << array_reverse.reverse
+    array_reverse << array.reverse
  
   end
 end
